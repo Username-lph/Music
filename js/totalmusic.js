488ba1dc6f4c5b0d8e60c3c21357a1b5fd119d7b
Username-lph <120899698@qq.com>
2016-11-07 09:56:42 +0800

    html

--- js/totalmusic.js
+++ js/totalmusic.js
@@ -26,27 +26,32 @@ $(document).ready(function() {
 		name: "黄国伦 - 直到世界都哭泣",
 		author: "黄国伦",
 		src: "music/黄国伦 - 直到世界都哭泣.mp3",
+		p:"<p>直到世界都哭泣</p><p>黄国伦</p>",
 		content:"<li>直到世界都哭泣</li><li>黄国伦</li><li>电影《极光之爱》泪光主题曲</li><li>︿☆直到世界都哭泣☆︿</li><li>演唱：黄国伦</li><li>作词：黄国伦 作曲：黄国伦</li><li>爱上你 是早已注定的宿命</li><li>我无力抗拒陷入你的柔情</li><li>直到不能呼吸</li><li>忘记你 是夜夜徒劳的练习</li><li>我疯狂用尽力气 想离开你</li><li>但不能 命运将我们绑得更紧</li><li>就让我这样爱着你</li><li>就算是场悲剧</li><li>天若有情天亦老</li><li>你会知道 我的心痛</li><li>就让我这样爱着你</li><li>永远不离开你</li><li>一路陪你到尽头</li><li>无悔无求 直到世界都哭泣</li><li>我依然会相信真爱有奇迹</li><li>忘记你 是夜夜徒劳的练习</li><li>我疯狂用尽力气 想离开你</li><li>但不能 命运将我们绑得更紧</li><li>就让我这样爱着你</li><li>就算是场悲剧</li><li>天若有情天亦老</li><li>你会知道 我的心痛</li><li>就让我这样爱着你</li><li>永远不离开你</li><li>一路陪你到尽头</li><li>无悔无求 直到世界都哭泣</li><li>我依然会相信真爱有奇迹</li><li>我依然会相信真爱有奇迹</li>"
 	},	{
 		name:"巫启贤 - 不该让你等太久",
 		author:"巫启贤",
 		src:"巫启贤 - 不该让你等太久.mp3",
+		p:"<p>不该让你等太久</p><p>巫启贤</p>",
 		content:"<li>歌手： 巫启贤</li><li>所属专辑：《太傻 情歌回顾展》</li><li>不该让你等太久 - 巫启贤</li><li>为何窗前只能下着雨</li><li>带着冷风拨乱了思绪</li><li>日子越来越没有意义</li><li>是否知道我每天都想你</li><li>只有自己陪伴着影子</li><li>是否见面只在入梦以后</li><li>不变的距离让彼此难受</li><li>孤枕独眠的夜还要多久</li><li>把你想个够</li><li>不要再担忧</li><li>我知道不该让你等太久</li><li>所有天的遥远地的沉默</li><li>海的辽阔山的冷漠</li><li>可有改变你的心</li><li>还有深夜的露思念的泪</li><li>等待的苦岁月的酒</li><li>可有醉了你容颜</li><li>那些空的承诺甜的言语</li><li>游戏的人虚假的心</li><li>可有动摇你的心</li><li>不管多少折磨多久守候</li><li>对你的爱依然不变</li><li>只有自己陪伴着影子</li><li>是否见面只在入梦以后</li><li>不变的距离让彼此难受</li><li>孤枕独眠的夜还要多久</li><li>把你想个够</li><li>不要再担忧</li><li>我知道不该让你等太久</li><li>所有天的遥远地的沉默</li><li>海的辽阔山的冷漠</li><li>可有改变你的心</li><li>还有深夜的露思念的泪</li><li>等待的苦岁月的酒</li><li>可有醉了你容颜</li><li>那些空的承诺甜的言语</li><li>游戏的人虚假的心</li><li>可有动摇你的心</li><li>不管多少折磨多久守候</li><li>对你的爱依然不变</li><li>那些空的承诺甜的言语</li><li>游戏的人虚假的心</li><li>可有动摇你的心</li><li>不管多少折磨多久守候</li><li>对你的爱依然不变</li>"
 	},	{	
 		name: '陈小春 - 独家记忆',
 		author: '陈小春',
 		src: 'music/陈小春 - 独家记忆.mp3',
+		p:"<p>独家记忆</p><p>陈小春</p>",
 		content:"<li>独家记忆</li><li>专辑：《独家记忆》</li><li>演唱：陈小春</li><li>忘记分开后的第几天起</li><li>喜欢一个人 看下大雨</li><li>没联络 孤单就像连锁反应</li><li>想要快乐都没力气</li><li>雷雨世界像场灾难电影</li><li>让现在的我 可怜到底</li><li>对不起 谁也没有时光机器</li><li>已经结束的 没有商量的余地</li><li>我希望你 是我独家的记忆</li><li>不管别人说的多么难听</li><li>现在我拥有的事情</li><li>是你 是给我一半的爱情</li><li>我喜欢你 是我独家的记忆</li><li>谁也不行</li><li>从我这个身体中拿走你</li><li>在我感情的封锁区</li><li>有关于你 绝口不提 没问题</li><li>雷雨世界像场灾难电影</li><li>让现在的我 可怜到底</li><li>对不起 谁也没有时光机器</li><li>已经结束的 没有商量的余地</li><li>我希望你 是我独家的记忆</li><li>摆在心底</li><li>不管别人说的多么难听</li><li>现在我拥有的事情</li><li>是你 是给我一半的爱情</li><li>谁也不行</li><li>从我这个身体中拿走你</li><li>在我感情的封锁区</li><li>有关于你 绝口不提 没关系</li><li>我希望你 是我独家的记忆</li><li>摆在心底</li><li>不管别人说的多么难听</li><li>现在我拥有的事情</li><li>是你 是给我一半的爱情</li><li>我喜欢你 是我独家的记忆</li><li>谁也不行</li><li>从我这个身体中拿走你</li><li>在我感情的封锁区</li><li>有关于你 绝口不提 没限期</li>"
 		
 	}, {
 		name: "金南玲 - 逆流成河",
 		author: "金南玲",
 		src: "music/金南玲 - 逆流成河.mp3",
+		p:"<p>逆流成河</p><p>金南玲</p>",
 		content:"<li>逆流成河</li><li>词曲：廖伟志</li><li>演唱：金南玲</li><li>斑驳的夜色在说什么</li><li>谁能告诉我如何选择</li><li>每当我想起分离时刻</li><li>悲伤就逆流成河</li><li>你给的温暖属于谁呢</li><li>谁又会在乎我是谁呢</li><li>每当我想起你的选择</li><li>悲伤就逆流成河</li><li></li><li>失去了你也是种获得</li><li>一个人孤单未尝不可</li><li>每当我深夜辗转反侧</li><li>悲伤就逆流成河</li><li></li><li>离开你也是一种快乐</li><li>没人说一定非爱不可</li><li>想问你双手是否温热</li><li>悲伤就逆流成河</li><li>我想是因为我太天真</li><li>难过是因为我太认真</li><li>每当我想起你的眼神</li><li>悲伤就逆流成河</li>"
 	},	{
 		name:"巫启贤 - 人生如梦",
 		author:"巫启贤",
 		src:"巫启贤 - 人生如梦.mp3",
+		p:"<p>人生如梦</p><p>巫启贤</p>",
 		content:"<li>歌手： 巫启贤</li><li>发行时间：1993-12-01</li><li>所属专辑：《红尘来去一场梦》</li><li>人生如梦-巫启贤</li><li>当我开始活的像一阵风</li><li>在人间不停的吹送</li><li>走过了春夏卷过了秋冬</li><li>用看戏的眼看待繁华梦</li><li>渐渐我发觉人生就像梦</li><li>像蝴蝶穿越了时空</li><li>有多少真实有几分朦胧</li><li>有苦有乐有笑有痛</li><li>人皆寻梦 梦里不分西东</li><li>醒在红尘中 醉在红尘中</li><li>何不从从容容入梦</li><li>人皆寻梦 梦里不分西东</li><li>爱在红尘中 恨在红尘中</li><li>何不潇洒入梦</li><li>日升日落潮起潮涌</li><li>梦里有爱何必怕人去楼空</li><li>当我开始活得像一阵风</li><li>在人间不停的吹送</li><li>走过了悲欢卷过了聚散</li><li>梦如人生人生如梦</li><li>人皆寻梦 梦里不分西东</li><li>醒在红尘中 醉在红尘中</li><li>何不从从容容入梦</li><li>人皆寻梦 梦里不分西东</li><li>爱在红尘中 恨在红尘中</li><li>何不潇洒入梦</li><li>日升日落潮起潮涌</li><li>梦里有爱何必怕人去楼空</li><li>当我开始活得像一阵风</li><li>在人间不停的吹送</li><li>走过了悲欢卷过了聚散</li><li>梦如人生人生如梦</li><li>走过了悲欢卷过了聚散</li><li>梦如人生人生如梦</li>"
 	}];
 	//将秒数转为时间
@@ -67,14 +72,12 @@ $(document).ready(function() {
 			var c = (i === current) ? "active" : "";
 			$('<li class="' + c + '"><span>' + v.name + '</span><span class="author">' + v.author + '</span></li>').appendTo("#music-list");
 		});
+		//歌曲歌词对应切换
 		$("#content").empty();
-//		$.each(music,function(i,v){
-//			var con = (i === current) ? "contentplay" : "";
-			$('<ul class="content">'+music[current].content+'</ul>').appendTo("#content");
-//		});
+		$('<ul class="content">'+music[current].content+'</ul>').appendTo("#content");
+		//歌曲顶部歌名对应
 		$(".song").empty();
-		var index = $(this).index;
-		console.log(index);
+		$('<div>'+music[current].p+'</div>').appendTo(".song");
 	}
 	$("#music-list").on("click", "li", function() {
 		$("#music-list").find("li").removeClass('active');
@@ -83,6 +86,7 @@ $(document).ready(function() {
 		audio.src = music[current].src;
 		audio.play();
 	});
+	
 	render();
 //	$audio.on("timeupdate",function(){
 //		$("#content").find("li").removeClass("contentplay");
@@ -262,13 +266,18 @@ $(document).ready(function() {
 		$("#content .content").css("top","120"-height);
 		var index = Math.floor(($(".content li").length) * audio.currentTime / audio.duration);
 		$(".content li").css("color","whitesmoke").eq(index).css("color","#FF0000");
+		//
+//		var song = $(".song");
+//		var hide=$(this).index();
+//		console.log(hide);
+//		$(".song div").hide().siblings().hide();
 	});
 	//播放和暂停
 	play.on("click","a",function() {
 		if(audio.paused) {
 			$("#hand img").addClass("hand");
 			rotate.addClass("play");
-			rotateimg.addClass("play");			
+			rotateimg.addClass("play");
 			audio.play();	
 		} else {
 			$("#hand img").removeClass("hand");
